41da93a9437a3e6bb084c8975820cc0ec2d3d3e4
Marcelo Amaral Alves <marceloaa@live.com>
2024-03-27 00:52:58 -0300

    porfolio is completed

--- backup-portfolio/src/Components/Intro/Intro.jsx
+++ backup-portfolio/src/Components/Intro/Intro.jsx
@@ -1,28 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Intro.css';
-import github from '../../img/github_icon.png'
-import linkedin from '../../img/linkedin _icon.png'
+import github from '../../img/github_icon.png';
+import linkedin from '../../img/linkedin _icon.png';
 import avatar from '../../img/avatar12.png';
 
-const scrollToContact = () => {
-  const contactSection = document.getElementById('contact');
-  if (contactSection) {
-    contactSection.scrollIntoView({ behavior: 'smooth' });
-  }
-};
-
 const Intro = () => {
+  const [typedText, setTypedText] = useState('');
+  const texts = ["iOS Developer ", "Fullstack Dev"];
+  const [textIndex, setTextIndex] = useState(0);
+  const [charIndex, setCharIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (charIndex < texts[textIndex].length) {
+        setTypedText(prevText => prevText + texts[textIndex][charIndex]);
+        setCharIndex(prevIndex => prevIndex + 1);
+      } else {
+        clearInterval(interval);
+      }
+    }, 200); // Intervalo para uma transição mais suave
+
+    return () => clearInterval(interval);
+  }, [textIndex, charIndex, texts]);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setTextIndex(prevIndex => (prevIndex + 1) % texts.length);
+      setTypedText(''); // Limpa o texto digitado para exibir o próximo texto
+      setCharIndex(0); // Reinicia o índice de caracteres para exibir o próximo texto
+    }, 2000); // Altera o texto a cada 3 segundos
+
+    return () => clearTimeout(timeout);
+  }, [textIndex, texts]);
+
+  const scrollToContact = () => {
+    const contactSection = document.getElementById('contact');
+    if (contactSection) {
+      contactSection.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
     <div className="intro">
       <div className="i-left">
         <div className="i-name">
           <span>Hello, I am</span>
           <span>Marcelo Amaral</span>
-          <span className='role'>iOS Developer </span>
+          <div className="role-container">
+            <span className='role'>{typedText}</span> {/* Renderiza o texto gradualmente */}
+          </div>
         </div>
         <div className="i-buttons-container">
           <button className="button i-button">Download CV</button>
-          {/* Usando a função scrollToContact para rolar até a seção de contato */}
           <button className="button-secondary i-button" onClick={scrollToContact}>Contact Info</button>
         </div>
         <div className="i-icons">
