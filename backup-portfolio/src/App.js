7f7d17b4ba24906c599fdc582dccb518a0cba462
Marcelo Amaral Alves <marceloaa@live.com>
2024-03-31 19:37:05 -0300

    lonk

--- backup-portfolio/src/App.js
+++ backup-portfolio/src/App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
 import Navbar from "./Components/Navbar/Navbar"
 import './App.css';
 import Intro from "./Components/Intro/Intro"
@@ -11,6 +13,13 @@ import Footer from "./Components/Footer/Footer";
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <meta property="og:title" content="Marcelo Amaral - DEV" />
+        <meta property="og:description" content="iOS/Fullstack Developer" />
+        <meta property="og:image" content="/Users/marcelovsk1/Desktop/backup-portfolio/backup-portfolio/src/img/logo512.png" />
+        <meta property="og:url" content="https://marceloaa.dev" />
+        <meta property="og:type" content="website" />
+      </Helmet>
       <Navbar/>
       <Intro />
       <About />
